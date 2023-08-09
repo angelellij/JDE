8bd3a837cc1dda56f81bad43adfa2891505007af
Javier Angelelli <angelellij@gmail.com>
2023-08-09 00:43:27 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
+#variables
+USERNAME="javier"
+BLOAT=true
+
+#If bloat is true you can finetune some apps to not install
+CODE=true
+DISCORD=true
+GIMP=true
+
+set -x
+
+apt-get install sudo
+/sbin/adduser $USERNAME sudo
+
 apt-get update
 apt-get upgrade
-apt-get install lightdm alacritty nemo code google-chrome-stable awesome fonts-roboto rofi compton lxappearance xbacklight flameshot xfce4-power-manager pnmixer network-manager-gnome policykit-1-gnome -y
+apt-get install lightdm alacritty nemo code chromium awesome fonts-roboto rofi compton lxappearance xbacklight flameshot xfce4-power-manager pnmixer network-manager-gnome policykit-1-gnome --ignore-missing -y
+
+if [ "$BLOAT" = true ]; then
+	if [ "$CODE" = true ]; then
+		wget -O ~/d.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
+		dpkg -i ~/d.deb
+		rm ~/d.deb
+	fi
+
+	if [ "$DISCORD" = true ]; then
+		wget -O ~/d.deb https://discord.com/api/download?platform=linux&format=deb
+		dpkg -i ~/d.deb
+		rm ~/d.deb
+	fi
+
+	if [ "$GIMP" = true ]; then
+		wget -O ~/d.deb https://discord.com/api/download?platform=linux&format=deb
+		dpkg -i ~/d.deb
+		rm ~/d.deb
+	fi
+fi
 systemctl start lightdm
 
-mkdir -p ~/.config/rofi
+mkdir -p /home/$USERNAME/.config/rofi
+mkdir -p /home/$USERNAME/.config/nemo
 
-rm ./.config/awesome
-rm ./.config/rofi
+rm -p /home/$USERNAME/.config/rofi
+rm -p /home/$USERNAME/.config/nemo
 
-cp -r ./JDE/awesome ./.config
-cp -r ./JDE/rofi ./.config
+cp -r ./JDE/awesome /home/$USERNAME/.config
+cp -r ./JDE/rofi /home/$USERNAME/.config
 
 rm -r JDE
---
+++ readme.md
@@ -0,0 +1,18 @@
+Work in progress...
+
+## **What is JDE**
+
+This is just a desktop environment i made for myself to install on any debian minimal installation. It uses awesome as a WM.
+
+## **Installation**
+
+When doing nano on install.sh change the username variable to your username.
+
+```
+su
+apt install git
+git clone https:github.com/angelellij/JDE
+nano ./JDE/install.sh
+chmod +x ./JDE/install.sh
+./JDE/install.sh
+```
