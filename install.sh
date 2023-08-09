c5342dcd9c4efb6bb95c1fda18f8929313b212e7
Javier Angelelli <angelellij@gmail.com>
2023-08-08 23:12:38 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ apt-get update
 apt-get upgrade
 apt install lightdm alacritty nemo code google-chrome-stable awesome fonts-roboto rofi compton lxappearance xbacklight flameshot xfce4-power-manager pnmixer network-manager-gnome policykit-1-gnome -y
 systemctl start lightdm
+
 mkdir -p ~/.config/rofi
-cp $HOME/.config/awesome/theme/config.rasi ~/.config/rofi/config.rasi
-sed -i '/@import/c\@import "'$HOME'/.config/awesome/theme/sidebar.rasi"' ~/.config/rofi/config.rasi
+
+rm ~/.config/awesome
+rm ~/.config/rofi
+
+cp -r $HOME/JDE/awesome ~/.config
+cp -r $HOME/JDE/rofi ~/.config
